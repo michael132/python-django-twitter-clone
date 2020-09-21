600affdd96e609f7cb4c2f2a1d4e802de77dcd7d
yuchili <yuchili@tesla.com>
2020-09-20 17:24:30 -0700

    Associate users to tweet and added user to admin panl

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-
 from .models import Tweet
 
-admin.site.register(Tweet)
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    search_fields = ['user__username', 'user__email']
+    list_display = ['__str__', 'user']
+
+    class Meta:
+        model = Tweet
